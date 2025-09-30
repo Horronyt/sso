b25ae0414278f5e5378304507e01cde084fbffa4
Horron <sashaobraztsovru@gmail.com>
2025-09-30 10:43:03 +0300

    Initial commit Сделал основу до написания хэндлеров

---
+++ cmd/sso/main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"log/slog"
+	"os"
+	"os/signal"
+	"sso/internal/app"
+	"sso/internal/config"
+	"syscall"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
+)
+
+func main() {
+	cfg := config.MustLoad()
+
+	log := setupLogger(cfg.Env)
+
+	log.Info("starting server", slog.Any("config", cfg))
+
+	application := app.New(log, cfg.GRPC.Port, cfg.StoragePath, cfg.TokenTTL)
+
+	go application.GRPCServer.MustRun()
+
+	// TODO: gRPC-сервер приложения
+
+	// Graceful shutdown
+
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, syscall.SIGTERM, syscall.SIGINT)
+
+	sign := <-stop
+
+	log.Info("received shutdown signal", slog.String("signal", sign.String()))
+
+	application.GRPCServer.Stop()
+
+	log.Info("application stopped")
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+
+	return log
+}
---
+++ internal/app/app.go
@@ -0,0 +1,21 @@
+package app
+
+import (
+	"log/slog"
+	grpcapp "sso/internal/app/grpc"
+	"time"
+)
+
+type App struct {
+	GRPCServer *grpcapp.App
+}
+
+func New(log *slog.Logger, grpcPort int, storagePath string, toketTTL time.Duration) *App {
+	// TODO:инициализировать хранилище
+
+	// TODO: init auth service
+
+	grpcApp := grpcapp.New(log, grpcPort)
+
+	return &App{GRPCServer: grpcApp}
+}
